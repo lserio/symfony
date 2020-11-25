a115705dc567f3df508129cb3166d3af3e3db201
Luca <luca.serio.dev@gmail.com>
2020-11-25 19:43:04 +0100

    Method like/unlike

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Entity\User;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 
 class MainController extends AbstractController
 {
@@ -31,6 +32,44 @@ class MainController extends AbstractController
         ]); */
 
         /* return new Response('<h1>PROVA</h1>'); */
+    }
+
+    /**
+     * @Route("/post/{post}/like", name="like_video", methods={"GET"})
+     * @Route("/post/{post}/unlike", name="undo_like_video", methods={"GET"})
+     */
+    public function toggleLikesAjax(Post $post, Request $request)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+
+        switch($request->get('_route')){
+            case 'like_video':
+
+                $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
+                $user->addLike($post);
+        
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+        
+                $result = 'like';
+
+            break;
+
+            case 'undo_like_video':
+                $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
+                $user->removeLike($post);
+        
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+        
+                $result = 'unlike';
+            break;
+
+        }
+
+        return $this->json(['action' => $result, 'id' => $post->getId()]);
     }
 
 }
